2cb6c29fc85d15cba15d0c06b51cdf0aa7837c93
Luzaks <kanemekanik@gmail.com>
2020-07-23 11:51:56 -0500

    Feat: Create Redux store. Initialize router and root reducer.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './Redux/store/store';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
