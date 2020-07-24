98d6a55b6170dfb615ad24aa2e5ec7f91a540e75
Luzaks <kanemekanik@gmail.com>
2020-07-23 23:57:34 -0500

    Feat: Add layout for filtering cameras.

--- src/containers/PhotosList.js
+++ src/containers/PhotosList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useSelector } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
@@ -17,14 +17,25 @@ const Img = styled.img`
 `;
 
 const PhotosList = () => {
-
+  const [cam, set_cam] = useState('');
   const rover = useSelector(state => state.rover);
+  const cameras = ['ALL', 'FHAZ', 'RHAZ', 'MAST', 'CHEMCAM', 'MAHLI', 'MARDI', 'NAVCAM', 'PANCAM', 'MINITES'];
 
   return (
     <Card>
-      {
-        rover.photos.map(item => <Img src={item.img_src} alt={`rover-${rover.rover_name}`} />)
-      }
+      <div>
+        <select className="filter-selector" name="filter" onChange={(ev) => {
+          set_cam(ev.target.value);
+        }}>
+          <option value="Categories" selected disabled hidden>CAMERAS</option>
+          { cameras.map(camera => <option key={camera} value={camera}>{camera}</option>)}
+        </select>
+      </div>
+      <Card>
+        {
+          rover.photos.map(item => <Img src={item.img_src} alt={`rover-${rover.rover_name}`} />)
+        }
+      </Card>
     </Card>
   );
 };
