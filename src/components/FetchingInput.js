25573e5adbdf2cc14b9c739ba820e6e50dd01e26
Luzaks <kanemekanik@gmail.com>
2020-07-24 15:54:04 -0500

    Feat: Add lifting state to Fetching rover input.

---
+++ src/components/FetchingInput.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const FetchingInput = ( {setRover, rovers }  ) => {
+  return (
+    <select className="filter-selector" name="filter"
+            onChange={
+              (ev) => {
+                setRover(ev.target.value);
+              }
+            }>
+      <option value="Rovers" selected disabled hidden>Rovers</option>
+        {
+          rovers.map(rover =>
+            <option key={rover} value={rover}>
+              {rover}
+            </option>)
+        }
+    </select>
+  );
+};
+
+export default FetchingInput;
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -4,6 +4,7 @@ import fetch_api from '../Redux/actions/actions';
 import PhotosList from './PhotosList';
 import { camerasCreator } from '../Redux/actions/actions';
 import { InputRow, ImgsRow } from '../styles/StyledComponents';
+import FetchingInput from '../components/FetchingInput';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
@@ -14,43 +15,20 @@ const Main = () => {
   const dispatch = useDispatch();
   const rovers = ['curiosity', 'opportunity', 'spirit'];
 
-    const cur_set = ['ALL', 'FHAZ', 'RHAZ', 'MAST', 'CHEMCAM' , 'MAHLI', 'MARDI', 'NAVCAM'];
-    const cams_set = ['ALL', 'FHAZ', 'RHAZ', 'NAVCAM', 'PANCAM', 'MINITES'];
+  const cur_set = ['ALL', 'FHAZ', 'RHAZ', 'MAST', 'CHEMCAM' , 'MAHLI', 'MARDI', 'NAVCAM'];
+  const cams_set = ['ALL', 'FHAZ', 'RHAZ', 'NAVCAM', 'PANCAM', 'MINITES'];
 
-    const uiHelper = (roverName) => {
-        if (roverName === 'curiosity') dispatch(camerasCreator(cur_set));
-        else dispatch(camerasCreator(cams_set))
-    };
+  const uiHelper = (roverName) => {
+    if (roverName === 'curiosity') dispatch(camerasCreator(cur_set));
+    else dispatch(camerasCreator(cams_set))
+  };
 
-/*
-
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch_api('curiosity');
-      dispatch(response);
-    }
-    fetchData().then(r => r);
-  }, [dispatch]);
-*/
     return (
       <div className="container">
         <InputRow className="row col-12">
-            <select className="filter-selector" name="filter"
-                    onChange={
-                        (ev) => {
-                        set_rover(ev.target.value);
-                    }}>
-                <option value="Rovers" selected disabled hidden>Rovers</option>
-                {
-                    rovers.map(rover =>
-                        <option key={rover} value={rover}>
-                            {rover}
-                        </option>)
-                }
-            </select>
-            <label htmlFor="sol">Sol:</label>
-            <input type="number" id="sol" name="sol" min="0" max="1000"
+          <FetchingInput setRover={set_rover} rovers={rovers} />
+          <label htmlFor="sol">Sol:</label>
+          <input type="number" id="sol" name="sol" min="0" max="1000"
                    value={ sol }
                    onChange={
                        (ev) => {
