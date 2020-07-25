a0061ddfc5a49d576b409a9321bad3183a1bc09d
Luzaks <kanemekanik@gmail.com>
2020-07-25 15:53:18 -0500

    Feat: Add first layout for item presentation.

--- src/components/itemComponents/Item.js
+++ src/components/itemComponents/Item.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Item = ({img}) => {
-    return (
-        <div>
-            {console.log(img)}
-            Item
-        </div>
-    );
+const mapStateToProps = state => ({ photo: state });
+
+class Item extends Component {
+    render() {
+        const { photo } = this.props;
+        return (
+            <div className="item-container d-flex flex-column">
+                <nav className="item-nav-bar">
+                    <ul>
+                        <Link to={{pathname: '/'}}>
+                            <li>Go back</li>
+                        </Link>
+                    </ul>
+                </nav>
+                <div className="item-info-container d-flex">
+                    <div className="left item-description d-flex align-items-center justify-content-center">
+                        left
+                    </div>
+                    <div className="right item-photo d-flex align-items-center justify-content-center">
+                        right
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+Item.propTypes = {
+    photo: PropTypes.object.isRequired,
 };
 
-export default Item;
+export default connect(mapStateToProps)(Item);
--- src/components/photoComponents/LinkPhoto.js
+++ src/components/photoComponents/LinkPhoto.js
@@ -4,17 +4,18 @@ import { photosCreator } from '../../Redux/actions/actions';
 import { Link } from 'react-router-dom';
 
 const LinkPhoto = ({ img, dispatch }) => {
-    return (
-        <div className="photo-hover-layer">
-            <Link to={{pathname: `/item/${img.id}`}}
-                  onClick={() => {
-                      dispatch(photosCreator(img))
-                  }}>
-                <p className="photo-hover-link">
-                    See details
-                </p>
-            </Link>
-        </div>
+  return (
+     <div className="photo-hover-layer">
+       <Link to={{pathname: `/item/${img.id}`}}
+             onClick={() => {
+               dispatch(photosCreator(img))
+               }
+             }>
+             <p className="photo-hover-link">
+               See details
+             </p>
+       </Link>
+     </div>
     );
 };
 
